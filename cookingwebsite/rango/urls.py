616cfe6f5b01849318584ad97c3fa510fe2d8b83
Lila <127430572+liliana-mrowiec@users.noreply.github.com>
2024-03-06 13:52:30 +0000

    All Files
    All Files

---
+++ cookingwebsite/rango/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rango import views
+
+app_name ='rango'
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('about/', views.about, name='about'),
+    path('api/openai/', views.openai_api, name='openai_api'),
+    path('profile/', views.profile, name='profile'),
+    path('catalogue/', views.catalogue, name='catalogue'),
+]
